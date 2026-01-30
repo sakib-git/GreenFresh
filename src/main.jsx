0c90c48b6e8d67c353be9b428701c5aafc744fc8
sakib-git <sakibulhasan32002@gmail.com>
2026-01-30 18:44:22 +0600

    refactor: clean up formatting in App.jsx and main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import { RouterProvider } from 'react-router';
@@ -9,11 +8,9 @@ import AuthProvider from './context/AuthProvider.jsx';
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <AuthProvider>
-        <RouterProvider router={router} />
-      </AuthProvider>
-    </QueryClientProvider>
-  </StrictMode>,
+  <QueryClientProvider client={queryClient}>
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
+  </QueryClientProvider>,
 );
