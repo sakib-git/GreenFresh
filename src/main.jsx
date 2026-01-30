a93a86e0492dd9c29774c039b8ea793721eb9faa
sakib-git <sakibulhasan32002@gmail.com>
2026-01-30 20:40:12 +0600

    feat: restructure routing by creating routes.jsx and updating import paths

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import { RouterProvider } from 'react-router';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { router } from './routes/route';
+import { router } from './routes/routes';
 
 const queryClient = new QueryClient();
 
