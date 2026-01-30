5a5eb13b2294ea55a09e88c8bc76332743e95dfe
sakib-git <sakibulhasan32002@gmail.com>
2026-01-30 20:57:17 +0600

    feat: create routes.jsx for routing configuration and update import in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import { RouterProvider } from 'react-router';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { router } from './routes/routes';
+import router from './routes';
 
 const queryClient = new QueryClient();
 
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,29 +1,29 @@
 import { createBrowserRouter } from 'react-router';
-import Home from '../pages/home/Home';
-import RootLayout from '../layout/RootLayout';
-import AuthLayout from '../layout/AuthLayout';
-import Register from '../pages/auth/Register';
-import LoveSave from '../pages/loveSave/LoveSave';
-import Addtocart from '../pages/addtoCart/Addtocart';
-import AllCategories from '../pages/allcategories/AllCategories';
-import About from '../pages/about/About';
-import Service from '../pages/service/Service';
-import Contact from '../pages/contact/Contact';
-import Dashboard from '../pages/dashboard/Dashboard';
-import DashboardLayout from '../layout/DashboardLayout';
-import Order from '../pages/dashboard/Order';
-import PrivateRoute from './PrivateRoute';
-import AuthPrivate from './AuthPrivate';
-import AdminDashboard from '../pages/admin/AdminDashboard';
-import ManageUser from '../pages/admin/ManageUser';
-import Error from '../components/Error';
-import ContactUser from '../pages/admin/ContactUser';
-import AddProduct from '../pages/admin/AddProduct';
-import ViewDetails from '../components/ViewDetails';
-import Login from '../pages/auth/Login';
-import AuthProvider from '../context/AuthProvider';
+import Home from './pages/home/Home';
+import RootLayout from './layout/RootLayout';
+import AuthLayout from './layout/AuthLayout';
+import Register from './pages/auth/Register';
+import LoveSave from './pages/loveSave/LoveSave';
+import Addtocart from './pages/addtoCart/Addtocart';
+import AllCategories from './pages/allcategories/AllCategories';
+import About from './pages/about/About';
+import Service from './pages/service/Service';
+import Contact from './pages/contact/Contact';
+import Dashboard from './pages/dashboard/Dashboard';
+import DashboardLayout from './layout/DashboardLayout';
+import Order from './pages/dashboard/Order';
+import PrivateRoute from './routes/PrivateRoute';
+import AuthPrivate from './routes/AuthPrivate';
+import AdminDashboard from './pages/admin/AdminDashboard';
+import ManageUser from './pages/admin/ManageUser';
+import Error from './components/Error';
+import ContactUser from './pages/admin/ContactUser';
+import AddProduct from './pages/admin/AddProduct';
+import ViewDetails from './components/ViewDetails';
+import Login from './pages/auth/Login';
+import AuthProvider from './context/AuthProvider';
 
-export const router = createBrowserRouter([
+const router = createBrowserRouter([
   {
     path: '/',
     element: (
@@ -128,3 +128,5 @@ export const router = createBrowserRouter([
     element: <Error></Error>,
   },
 ]);
+
+export default router;
