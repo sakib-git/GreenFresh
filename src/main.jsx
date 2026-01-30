cf1fbd5cef8046dff8d60dc4db5a7f830167ebaa
sakib-git <sakibulhasan32002@gmail.com>
2026-01-30 18:14:04 +0600

    f

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,8 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import { RouterProvider } from 'react-router';
 import { router } from './Routes/Route.jsx';
-import AuthProvider from './context/AuthProvider.jsx';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import AuthProvider from './context/AuthProvider.jsx';
 const queryClient = new QueryClient()
 createRoot(document.getElementById('root')).render(
   <StrictMode>
